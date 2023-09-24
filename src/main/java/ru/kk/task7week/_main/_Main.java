1dab2171d91aad032bb1569363b6565c6287a91c
EJIeKTpa <sawwaksu@yandex.ru>
2023-09-24 19:01:38 +0900

    1

--- src/main/java/ru/kk/task7week/_main/_Main.java
+++ src/main/java/ru/kk/task7week/_main/_Main.java
@@ -4,14 +4,21 @@ import ru.kk.task7week.service.Solution;
 
 import java.util.Arrays;
 
+import static ru.kk.task7week.service.BracketValidation.isValid;
+
 public class _Main {
     public static void main(String[] args) {
+//1
         int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
         int[] expectedNums = {0, 1, 2, 3, 4, 0, 0, 0, 0, 0};
-
         Solution solution = new Solution();
         int[] result = solution.removeDuplicates(nums);
-
         System.out.println(Arrays.equals(result, expectedNums));
+//2
+        String[] testCases = {"()", "()[]{}", "(]", "([)]"};
+        for (String testCase : testCases) {
+            System.out.println(testCase + ": " + isValid(testCase));
+        }
+
     }
 }
